e2a1b1dd74fa25064e1b242cecc81c487ba00938
Alexandra <Terentieva.Alexandra2020@yandex.ru>
2025-11-22 01:04:40 +0300

    COMMIT

--- Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcbd2101af3e6bcf3c6c66e4a3349b70825211ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d6ac954277013f0beb8e4b7974ee9d8dfbb6944")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
