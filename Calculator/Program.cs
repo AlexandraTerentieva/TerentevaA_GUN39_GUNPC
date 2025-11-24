c49a753ca069f75cb110b64197cffc2c6725b11b
Alexandra <Terentieva.Alexandra2020@yandex.ru>
2025-11-24 16:13:04 +0300

    Commit

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,44 +1,108 @@
 ﻿namespace HomeWork
 {
-    internal class Program
+    public class Unit
+    {
+        public string Name { get; }
+        public float Health => _health;
+        public int Damage { get; }
+        public float Armor { get; }
+
+        private float _health;
+
+        public Unit() : this("Unknown Unit")
+        {
+        }
+
+        public Unit(string name)
+        {
+            Name = name;
+            Damage = 5;
+            Armor = 0.6f;
+            _health = 100f;
+        }
+
+        public float GetRealHealth()
+        {
+            return Health * (1f + Armor);
+        }
+
+        public bool SetDamage(float value)
+        {
+            _health -= value * Armor;
+            return _health <= 0f;
+        }
+    }
+
+    public class Weapon
+    {
+        public string Name { get; }
+        public int MinDamage { get; private set; }
+        public int MaxDamage { get; private set; }
+        public float Durability { get; }
+
+        public Weapon(string name)
+        {
+            Name = name;
+            Durability = 1f;
+        }
+
+        public Weapon(string name, int minDamage, int maxDamage) : this(name)
+        {
+            SetDamageParams(minDamage, maxDamage);
+        }
+
+        public void SetDamageParams(int minDamage, int maxDamage)
+        {
+            if (minDamage > maxDamage)
+            {
+                (minDamage, maxDamage) = (maxDamage, minDamage);
+                Console.WriteLine($"Incorrect input data for weapon '{Name}'. Values swapped.");
+            }
+
+            if (minDamage < 1)
+            {
+                minDamage = 1;
+                Console.WriteLine($"Forced minimum value for weapon '{Name}'.");
+            }
+
+            if (maxDamage <= 1)
+            {
+                maxDamage = 10;
+            }
+
+            MinDamage = minDamage;
+            MaxDamage = maxDamage;
+        }
+
+        public int GetDamage()
+        {
+            return (MinDamage + MaxDamage) / 2;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Task 1 - First 10 Fibonacci numbers:");
-            int a = 0, b = 1;
-            Console.Write(a + ", " + b);
-            for (int i = 2; i < 10; i++)
-            {
-                int next = a + b;
-                Console.Write(", " + next);
-                a = b;
-                b = next;
-            }
-            Console.WriteLine();
-            Console.WriteLine("\nTask 2 - Even numbers from 2 to 20:");
-            for (int i = 2; i <= 20; i += 2)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("\nTask 3 - Multiplication table from 1 to 5:");
-            for (int i = 1; i <= 5; i++)
-            {
-                for (int j = 1; j <= 5; j++)
-                {
-                    Console.Write($"{i} * {j} = {i * j}\t");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("\nTask 4 - Password check:");
-            string password = "qwerty";
-            string input;
-            do
-            {
-                Console.Write("Enter password: ");
-                input = Console.ReadLine();
-            } while (input != password);
-            Console.WriteLine("Access granted!");
+            Unit unit1 = new Unit();
+            Unit unit2 = new Unit("Warrior");
+
+            Console.WriteLine($"Unit 1: {unit1.Name}, Health: {unit1.Health}, Damage: {unit1.Damage}, Armor: {unit1.Armor}");
+            Console.WriteLine($"Unit 2: {unit2.Name}, Health: {unit2.Health}, Damage: {unit2.Damage}, Armor: {unit2.Armor}");
+            Console.WriteLine($"Unit 2 Real Health: {unit2.GetRealHealth()}");
+
+            bool isDead = unit2.SetDamage(50f);
+            Console.WriteLine($"After taking 50 damage: Health = {unit2.Health}, Is Dead: {isDead}");
+
+            Weapon sword = new Weapon("Sword", 5, 15);
+            Weapon axe = new Weapon("Axe", 10, 5);
+            Weapon brokenWeapon = new Weapon("Broken", -5, 1);
+
+            Console.WriteLine($"Weapon: {sword.Name}, Damage: {sword.GetDamage()}, Range: {sword.MinDamage}-{sword.MaxDamage}");
+            Console.WriteLine($"Weapon: {axe.Name}, Damage: {axe.GetDamage()}, Range: {axe.MinDamage}-{axe.MaxDamage}");
+            Console.WriteLine($"Weapon: {brokenWeapon.Name}, Damage: {brokenWeapon.GetDamage()}, Range: {brokenWeapon.MinDamage}-{brokenWeapon.MaxDamage}");
         }
     }
 }
+
+
--- Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d6ac954277013f0beb8e4b7974ee9d8dfbb6944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a1b1dd74fa25064e1b242cecc81c487ba00938")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
